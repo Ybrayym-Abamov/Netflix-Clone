93db56c1eac6abd7ef3d4ee2c6b6f72dbdd07499
Ybrayym <ybrayymjan90@gmail.com>
2020-10-11 21:12:44 -0500

    Homepage is half way done

--- netflix/src/components/jumbotron/index.js
+++ netflix/src/components/jumbotron/index.js
@@ -3,8 +3,8 @@ import { Inner, Container, Pane, Item, Title, SubTitle, Image } from './styles/j
 
 export default function Jumbotron({ children, direction = 'row', ...restProps }) {
     return (
-        <Item direction={direction} { ...restProps }>
-            <Inner>{children}</Inner>
+        <Item { ...restProps }>
+            <Inner direction={direction}>{children}</Inner>
         </Item>
     )
 }
