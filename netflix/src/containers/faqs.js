990fff134029cd687cbbca312417ae812f81a325
Ybrayym <ybrayymjan90@gmail.com>
2020-10-16 12:18:35 -0500

    home page is complete

--- netflix/src/containers/faqs.js
+++ netflix/src/containers/faqs.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import { Accordion } from '../components';
 import faqsData from '../fixtures/faqs.json';
+import OptForm from '../components/opt-form';
 
 export function FaqsContainer() {
     return (
         <Accordion>
             <Accordion.Title>Frequently Asked Questions</Accordion.Title>
-            {faqsData.map(item => 
-                <Accordion.Item key={item.id}>
-                    <Accordion.Header>{item.header}</Accordion.Header>
-                    <Accordion.Body>{item.body}</Accordion.Body>
-                </Accordion.Item>)}
-            <Accordion.Item></Accordion.Item>
+            <Accordion.Frame>
+                {faqsData.map((item) => (
+                    <Accordion.Item key={item.id}>
+                        <Accordion.Header>{item.header}</Accordion.Header>
+                        <Accordion.Body>{item.body}</Accordion.Body>
+                    </Accordion.Item>
+                ))}
+            </Accordion.Frame>
+
+            <OptForm>
+                <OptForm.Break />
+                <OptForm.Text>
+                    Ready to watch? Enter your email to create or restart your membership.
+                </OptForm.Text>
+                <OptForm.Input placeholder="Email Adress" />
+                <OptForm.Button>GET STARTED</OptForm.Button>
+            </OptForm>
+
         </Accordion>
     )
 }
---
+++ netflix/src/pages/home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Feature, OptForm } from '../components';
+import { HeaderContainer } from '../containers/header';
+import { FaqsContainer } from '../containers/faqs';
+import { FooterContainer } from '../containers/footer';
+import { JumbotronContainer } from '../containers/jumbotron';
+
+export default function Home() {
+    return (
+        <>
+            <HeaderContainer>
+                <Feature>
+                    <Feature.Title>Unlimited movies, TV shows, and more.</Feature.Title>
+                    <Feature.SubTitle>Watch anywhere. Cancel anytime.</Feature.SubTitle>
+                    <OptForm>
+                        <OptForm.Input placeholder="Email Adress" />
+                        <OptForm.Button>GET STARTED</OptForm.Button>
+                        <OptForm.Break />
+                        <OptForm.Text>
+                            Ready to watch? Enter your email to create or restart your membership.
+                        </OptForm.Text>
+                    </OptForm>
+                </Feature>
+            </HeaderContainer>
+            <JumbotronContainer />
+            <FaqsContainer />
+            <FooterContainer />
+        </>
+    );
+}
