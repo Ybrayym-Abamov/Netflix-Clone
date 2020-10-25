1ee487bef31b0eb305ac09722e3f0080315607a9
Ybrayym <ybrayymjan90@gmail.com>
2020-10-25 01:49:13 -0500

    completed the browser page, finished the search functionality

--- netflix/src/lib/firebase.prod.js
+++ netflix/src/lib/firebase.prod.js
@@ -1,7 +1,6 @@
 import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-// import { seedDatabase } from '../seed'; DO NOT TOUCH THIS
 
 const config = {
     apiKey: "AIzaSyBEF8G2yzpZ1LlYDD4I9Z2x6b4lGkst29c",
@@ -15,6 +14,5 @@ const config = {
 
 const firebase = Firebase.initializeApp(config);
 
-// seedDatabase(firebase);   DO NOT TOUCH THIS
 
 export { firebase };
